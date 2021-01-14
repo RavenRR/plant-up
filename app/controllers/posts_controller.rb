b47104c57bb0ce7ab3f085e3fb64607f991b75af
romeroraven <romeroravenr@gmail.com>
2021-01-13 22:41:31 -0700

    Create Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
+
+    get '/posts/new' do
+        erb :'posts/new'
+
+    end
+
+
 get '/posts' do
     @posts = Post.all
     erb :'posts/index'
+    end
+
+get '/posts/:id' do 
+    @post = Post.find(params["id"])
+    erb :'posts/show'
+
     end
 end
