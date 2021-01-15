64bc089f6747aa0ac38d5401398caee8a02f95ae
romeroraven <romeroravenr@gmail.com>
2021-01-15 16:54:47 -0700

    Post controller update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,16 @@
 class PostsController < ApplicationController
 
+
     get '/posts/new' do
         erb :'/posts/new'
 
     end
-    
+
     post '/posts' do
         post = Post.new(params)
         if !post.title.empty? 
             post.save
             redirect '/posts'
-            
-
         else
             @error = "Invalid. Please enter again."
             erb :'/posts/new'
@@ -21,17 +20,27 @@ end
 
     get '/posts' do
         @posts = Post.all.reverse
-        erb :'/posts/index'
+        erb :'posts/index'
     end
 
     get '/posts/:id' do 
-        @posts = Post.find(params["id"])
-        erb :'/posts/show'
+        @posts = Post.find(params[:id])
+        erb :'posts/show'
     end
-
+    
     get '/posts/:id/edit' do
-        @posts = Post.find(params["id"])
+        @posts = Post.find(params[:id])
         erb :'/posts/edit'
     end
 
+    patch '/posts/:id' do
+        @posts = Post.find(params[:id])
+        if !params["post"]["title"].empty?
+            @posts.update(params["post"])
+            redirect "/posts"
+        else
+            @error = "Invalid. Please enter again."
+            erb :'/posts'
+        end
+    end
 end
