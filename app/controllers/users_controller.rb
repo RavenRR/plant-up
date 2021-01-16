5044a765c34029dc09344afb3f36dc21e66c851b
romeroraven <romeroravenr@gmail.com>
2021-01-15 21:39:30 -0700

    User signup  update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
-    #Sign up
     get '/signup' do
-        erb :'users/signup.erb'
+        erb :'users/signup'
+    end
 
-    # user delete account
+    post '/signup' do
+        user = User.new(params)
+        if user.username.empty? || user.password.empty?
+            @error = "Username and/or Password is blank"
+            erb :'users/signup'
+        elsif User.find_by(username: user.username)
+            @error = "Username already exists"
+            erb :'users/signup'
+        else
+            user.save
+            session[:user_id] = user.id
+            redirect '/posts'
+        end
+    end
+end
+
