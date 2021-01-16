02e2eb1fe97a3368f072de2151d95fb29d143aa6
romeroraven <romeroravenr@gmail.com>
2021-01-16 02:00:00 -0700

    Secure password

---
+++ db/migrate/20210116085711_chane_password_to_password_digest.rb
@@ -0,0 +1,6 @@
+class ChanePasswordToPasswordDigest < ActiveRecord::Migration
+  def change
+    remove_column :users, :password, :String
+    add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210112064127) do
+ActiveRecord::Schema.define(version: 20210116085711) do
 
   create_table "posts", force: :cascade do |t|
     t.string  "title"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20210112064127) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
